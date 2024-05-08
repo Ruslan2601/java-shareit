b97a6ec82844c6affa082d96181ba60b58f9d4ba
Ruslan2601 <Fhnehgblh2>
2024-05-08 19:06:15 +0300

    правки

--- server/src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
+++ server/src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.user.mapper;
 import org.mapstruct.Mapper;
 import ru.practicum.shareit.user.dto.UserCreate;
 import ru.practicum.shareit.user.dto.UserResponse;
-import ru.practicum.shareit.user.dto.UserUpdate;
 import ru.practicum.shareit.user.model.User;
 
 @Mapper(componentModel = "spring")
